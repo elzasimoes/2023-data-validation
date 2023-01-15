4c825fe899fb8a65db9cfb19c9fb175dfaa14c88
Elza Simoesl <elzaesimoes@gmail.com>
2023-01-15 14:53:43 -0300

    [NEW] #1 Validating CPF and accessing PyPi

---
+++ main.py
@@ -0,0 +1,16 @@
+from validate_docbr import CPF
+from Cpf import Cpf
+
+# cpf = "02410319238"
+
+# objeto_cpf = Cpf(cpf)
+
+# print(objeto_cpf)
+
+# cpf = CPF()
+# print(cpf.validate("024.103.192-38"))
+
+cpf_um = Cpf("15316264754")
+print(cpf_um)
+
+
